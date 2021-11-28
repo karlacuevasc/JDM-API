663039602594c215ea0550bba5454fb78db9a74e
karlacuevasc <karlacuevasdesigns@gmail.com>
2021-11-28 23:54:01 +0000

    Completed the set up for server and endpoint. Currently working on the cars.json file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const fs = require('fs')
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)
+)
+
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.send('Welcome! This API contains information about the most popular JDM vehicles dedicated to car enthusiasts. Visit /carlist to access the vehicle data.')
+  })
+
+app.get("/carlist", (req, res) => {
+    let carData = JSON.parse(fs.readFileSync('cars.json', 'utf-8'))
+  
+    res.json(carData)
+  })
